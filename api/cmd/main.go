58928a3668f11e23da54d2ec24aeab70c0b6eb95
Abush Natanem <abushnatanem@gmail.com>
2025-12-30 08:42:14 +0300

    feat: simple gin setup

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"github.com/klaus-creations/klaus-judge/api/internal/config"
 	"github.com/klaus-creations/klaus-judge/api/internal/routes"
 )
 
+
+func init () {
+	config.Connectdatabase()
+}
+
 func main() {
 	r := routes.SetupRouter()
-
-	fmt.Println("Server starting on port 8080...")
-	if err := r.Run(":8080"); err != nil {
-		panic(err)
-	}
+	r.Run(":8080")
 }
