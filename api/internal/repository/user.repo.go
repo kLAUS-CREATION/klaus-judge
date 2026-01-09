9ae33dbef0bdd75a7afd2b70cfa510885f3c7285
Abush Natanem <abushnatanem@gmail.com>
2026-01-09 23:40:57 +0300

    change

--- api/internal/repository/user.repo.go
+++ api/internal/repository/user.repo.go
@@ -2,7 +2,11 @@ package repository
 
 import "github.com/klaus-creations/klaus-judge/api/internal/domain"
 
+// UserRepository defines the interface for user data operations.
 type UserRepository interface {
 	Create(user *domain.User) error
 	FindByEmail(email string) (*domain.User, error)
+	FindByUsername(username string) (*domain.User, error)
+	FindByID(id uint) (*domain.User, error)
+	Update(user *domain.User) error
 }
