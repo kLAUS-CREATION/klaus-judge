aff011e254b9166f81e3cf776ae286f250d006b6
Abush Natanem <abushnatanem@gmail.com>
2026-01-09 18:02:37 +0300

    fix: update user model

--- api/internal/domain/user.go
+++ api/internal/domain/user.go
@@ -3,10 +3,16 @@ package domain
 import "time"
 
 type User struct {
-	ID           uint
-	FullName     string
-	Email        string
-	Username     string
-	PasswordHash string
-	CreatedAt    time.Time
+	ID            uint      `gorm:"primaryKey"`
+	FullName      string    `gorm:"not null"`
+	Email         string    `gorm:"uniqueIndex;not null"`
+	Username      string    `gorm:"uniqueIndex;not null"`
+	PasswordHash  string    `gorm:"not null"`
+	Role          string    `gorm:"default:'user'"`
+	Verified      bool      `gorm:"default:false"`
+	LastLogin     time.Time
+	SolvedProblems int       `gorm:"default:0"`
+	Rating        int       `gorm:"default:1000"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
 }
