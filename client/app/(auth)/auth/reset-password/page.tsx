13f06a584ebe91bfdd166ba2e04a871d73fbf0b8
Abush Natanem <abushnatanem@gmail.com>
2026-01-11 23:13:22 +0300

    feat: reset password page

---
+++ client/app/(auth)/auth/reset-password/page.tsx
@@ -0,0 +1,23 @@
+import ResetPasswordForm from "@/components/auth/reset-password-form";
+import { Metadata } from "next";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
+
+export const metadata: Metadata = {
+    title: "Reset Password | Klaus Judge",
+    description: "Set a new password",
+};
+
+export default function ResetPasswordPage() {
+    return (
+        <Suspense
+            fallback={
+                <div className="flex justify-center items-center h-full">
+                    <Loader2 className="h-8 w-8 animate-spin" />
+                </div>
+            }
+        >
+            <ResetPasswordForm />
+        </Suspense>
+    );
+}
