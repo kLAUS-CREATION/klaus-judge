6874c14b5bba51f939a33f8b8ebd041102baa3a0
Abush Natanem <abushnatanem@gmail.com>
2026-01-11 22:41:19 +0300

    update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AppProviders>
           <div className="size-full bg-background">{children}</div>
