ae4bf77a5233b891072ae155cd7d7743fde09188
Abush Natanem <abushnatanem@gmail.com>
2026-02-09 21:10:15 +0300

    create shared logo file

---
+++ client/components/shared/logo.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+import React from "react";
+export default function Logo () {
+    return (
+        <h1 className="bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent text-lg lg:text-xl first-letter:text-3xl tracking-[1px]">
+            <Link href="/">KlausCode</Link>
+        </h1>
+    )
+}
