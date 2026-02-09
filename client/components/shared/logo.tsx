1ccf11cf65dfef4d9164c59e4eba40a55d1ad3f4
Abush Natanem <abushnatanem@gmail.com>
2026-02-09 21:12:22 +0300

    change

--- client/components/shared/logo.tsx
+++ client/components/shared/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 export default function Logo () {
     return (
-        <h1 className="bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent text-lg lg:text-xl first-letter:text-3xl tracking-[1px]">
+        <h1 className="bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent text-lg lg:text-xl first-letter:text-3xl tracking-[2px] italic font-serif">
             <Link href="/">KlausCode</Link>
         </h1>
     )
