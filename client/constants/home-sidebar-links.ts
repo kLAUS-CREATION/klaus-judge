09e724bcea54351c3280a1be81b780ab995910fc
Abush Natanem <abushnatanem@gmail.com>
2026-02-13 16:28:38 +0300

    feat: update sidebar links

---
+++ client/constants/home-sidebar-links.ts
@@ -0,0 +1,23 @@
+interface IHomeSidebarLink {
+  name: string;
+  link: string;
+}
+
+export const HomeSidebarLinks: IHomeSidebarLink[] = [
+  {
+    name: "Home",
+    link: "/home",
+  },
+  {
+    name: "Learn",
+    link: "/home/learn",
+  },
+  {
+    name: "Problems",
+    link: "/home/problems",
+  },
+  {
+    name: "Contests",
+    link: "/home/contests",
+  },
+];
